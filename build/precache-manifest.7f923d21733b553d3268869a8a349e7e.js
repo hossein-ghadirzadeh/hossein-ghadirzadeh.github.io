5aefa335bbe29926490cc58d5b6e71d687b3f366
hossein-ghadirzadeh <hossein.ghadirzadeh@gmail.com>
2022-05-09 13:58:05 +0430

    chore: deploy react app to GitHub pages

---
+++ build/precache-manifest.7f923d21733b553d3268869a8a349e7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "619e91b96b7739739641",
+    "url": "/static/css/main.524321b9.chunk.css"
+  },
+  {
+    "revision": "619e91b96b7739739641",
+    "url": "/static/js/main.619e91b9.chunk.js"
+  },
+  {
+    "revision": "329c5e07764700867d7a",
+    "url": "/static/js/1.329c5e07.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d28f6994626478a232def314d0eead8f",
+    "url": "/index.html"
+  }
+];
